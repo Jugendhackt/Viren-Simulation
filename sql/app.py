ad35b55eff91b8de2c04819b4e848b130521449e
Connor <halil_ismail@yahoo.com>
2022-10-08 11:59:01 +0200

    fix(flask): typo, rout -> route

--- sql/app.py
+++ sql/app.py
@@ -9,7 +9,7 @@ def db_connection():
     conn = None
     try:
         conn = sqlite3.connect("books.sqlite")
-    except sqlite3.error as e:
+    except sqlite3.error as e: # pyright: ignore
         print(e)
     return conn
 
@@ -18,17 +18,17 @@ def db_connection():
 def getdata():
     return render_template("get_data.html")
 
-@app.rout("/putdata")
+@app.route("/putdata")
 def putdata():
     return render_template("put_data.html")
 
 @app.route("/books", methods=["GET", "POST"])
 def books():
     conn = db_connection()
-    cursor = conn.cursor()
+    cursor = conn.cursor() # pyright: ignore
 
     if request.method == "GET":
-        cursor = conn.execute("SELECT * FROM book")
+        cursor = conn.execute("SELECT * FROM book") # pyright: ignore
         books = [
             dict(id=row[0], author=row[1], language=row[2], title=row[3])
             for row in cursor.fetchall()
@@ -43,14 +43,14 @@ def books():
         sql = """INSERT INTO book (author, language, title)
                  VALUES (?, ?, ?)"""
         cursor = cursor.execute(sql, (new_author, new_lang, new_title))
-        conn.commit()
+        conn.commit() # pyright: ignore
         return f"Book with the id: 0 created successfully", 201
 
 
 @app.route("/book/<int:id>", methods=["GET", "PUT", "DELETE"])
 def single_book(id):
     conn = db_connection()
-    cursor = conn.cursor()
+    cursor = conn.cursor() # pyright: ignore
     book = None
     if request.method == "GET":
         cursor.execute("SELECT * FROM book WHERE id=?", (id,))
@@ -78,14 +78,14 @@ def single_book(id):
             "language": language,
             "title": title,
         }
-        conn.execute(sql, (author, language, title, id))
-        conn.commit()
+        conn.execute(sql, (author, language, title, id)) # pyright: ignore
+        conn.commit() # pyright: ignore
         return jsonify(updated_book)
 
     if request.method == "DELETE":
         sql = """ DELETE FROM book WHERE id=? """
-        conn.execute(sql, (id,))
-        conn.commit()
+        conn.execute(sql, (id,)) # pyright: ignore
+        conn.commit() # pyright: ignore 
         return "The book with id: {} has been ddeleted.".format(id), 200
 
 
