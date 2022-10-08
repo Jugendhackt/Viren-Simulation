eea8efbee0ee3f396100547f5cd04af4d1e93f3c
Connor <halil_ismail@yahoo.com>
2022-10-08 11:39:13 +0200

    fix(sql,flask): remove database connection when GETting route /

--- flask/main.py
+++ flask/main.py
@@ -1,12 +1,10 @@
 from flask import Flask, render_template
-import sqlite3
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    database = sqlite3.connect('sql/database.db')
-    return render_template('index.html', x=database.execute('SELECT * FROM users').fetchall())
+    return render_template('index.html')
 
 
 @app.route('/popup')
