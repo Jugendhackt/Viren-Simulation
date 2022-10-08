9230f7ddc56bc964f6ed75f9b0c3bac4a9664c21
datewat <thomaszimmer524@gmail.com>
2022-10-08 15:54:47 +0200

    copy stuff to templates in flask dir

---
+++ flask/templates/Desktop.js
@@ -0,0 +1,13 @@
+function Browser(){
+
+    
+}
+function Explorer(){
+
+    
+}
+function Email(){
+
+    
+}
+document.getElementById("Itag").src="file:///C:/Users/Dell/Desktop/Projekt/Viren-Simulation/desktop/Desktop.html"
