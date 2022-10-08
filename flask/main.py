7531e4975c075013bc2d62761d6da0357440f824
Connor <halil_ismail@yahoo.com>
2022-10-08 10:56:56 +0200

    feat(sql): add database & connection

--- flask/main.py
+++ flask/main.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
+import sqlite3
 
 app = Flask(__name__)
-
+database = sqlite3.connect('sql/database.db')
+database.row_factory = sqlite3.Row
+database.execute('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, results TEXT)')
 
 @app.route('/')
 def hello():
