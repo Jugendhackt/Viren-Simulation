5476310be5f6bc597fdf1bcfd34151a60a4b88df
Stefan1293 <stefan.gruenthal@gmail.com>
2022-10-08 11:57:21 +0200

    But

--- desktop/Seite.js
+++ desktop/Seite.js
@@ -1,9 +1,4 @@
 function Hallo(P1){
 
-    document.getElementById("First").innerHTML+=1   
+    
 }
-var ctx = document.getElementById("con").getContext("2d");
-
-ctx.beginPath();
-ctx.arc(95, 50, 40, 0, 2 * Math.PI);
-ctx.stroke();
