f994dfbaeafe4bdaac514f3048e0ae61101de1ba
Connor <halil_ismail@yahoo.com>
2022-10-08 11:34:40 +0200

    refactor(sql): added database.sql, and ways on how to import that

--- sql/database.sql
+++ sql/database.sql
@@ -1,35 +1,7 @@
-/*
-*********************************************************************
-http://www.mysqltutorial.org
-*********************************************************************
-Name: MySQL Sample Database classicmodels
-Link: http://www.mysqltutorial.org/mysql-sample-database.aspx
-Version 3.1
-+ changed data type from DOUBLE to DECIMAL for amount columns
-Version 3.0
-+ changed DATETIME to DATE for some colunmns
-Version 2.0
-+ changed table type from MyISAM to InnoDB
-+ added foreign keys for all tables 
-*********************************************************************
+/* To import this, open the database (.db) file in SQLite, then run the following command:
+   source sql/database.sql
+
+   You can also use this if you're lazy:
+    sqlite3 database.db < sql/database.sql
 */
-
-
-/*!40101 SET NAMES utf8 */;
-
-/*!40101 SET SQL_MODE=''*/;
-
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-/*Table structure for table `customers` */
-
-DROP TABLE IF EXISTS `customers`;
-
-CREATE TABLE `users` (
-  `id` int NOT NULL,
-  `results` int NOT NULL,
-  PRIMARY KEY (`id`),
-) ENGINE=SQLite3 DEFAULT CHARSET=utf8;
+create table users(id init,results int(15));
