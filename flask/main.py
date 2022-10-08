1a859b484d2321d11f64306541129362fab80c26
Connor <halil_ismail@yahoo.com>
2022-10-08 11:23:18 +0200

    feat(sql): stuff

--- flask/main.py
+++ flask/main.py
@@ -2,13 +2,12 @@ from flask import Flask, render_template
 import sqlite3
 
 app = Flask(__name__)
-database = sqlite3.connect('sql/database.db')
-database.row_factory = sqlite3.Row
-database.execute('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, results TEXT)')
 
 @app.route('/')
 def hello():
-    return render_template("index.html")
+    database = sqlite3.connect('sql/database.db')
+    return render_template('index.html', x=database.execute('SELECT * FROM users').fetchall())
+
 
 @app.route('/popup')
 def popup():
---
+++ sql/database.sql
@@ -0,0 +1,35 @@
+/*
+*********************************************************************
+http://www.mysqltutorial.org
+*********************************************************************
+Name: MySQL Sample Database classicmodels
+Link: http://www.mysqltutorial.org/mysql-sample-database.aspx
+Version 3.1
++ changed data type from DOUBLE to DECIMAL for amount columns
+Version 3.0
++ changed DATETIME to DATE for some colunmns
+Version 2.0
++ changed table type from MyISAM to InnoDB
++ added foreign keys for all tables 
+*********************************************************************
+*/
+
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+/*Table structure for table `customers` */
+
+DROP TABLE IF EXISTS `customers`;
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `results` int NOT NULL,
+  PRIMARY KEY (`id`),
+) ENGINE=SQLite3 DEFAULT CHARSET=utf8;
