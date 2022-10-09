f3688bd937790df455aa8d9c7a6d6947e35abfc3
datewat <thomaszimmer524@gmail.com>
2022-10-09 08:54:04 +0200

    fix

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-echo -e "\e[1;42m[ARIVAAA] Starting setup"
+echo -e "\e[1;42m[ARIVA] Starting setup"
 
 python3.9 -m pip install -r requirements.txt
 git fetch
 
-echo -e "\e[1;42m[ARIVAAA] Setup complete"
+echo -e "\e[1;42m[ARIVA] Setup complete"
 
-echo -e "\e[1;42m[ARIVAAA] Starting server"
+echo -e "\e[1;42m[ARIVA] Starting server"
 python3.9 flask/app.py
